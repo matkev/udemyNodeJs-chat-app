489a765c2d28266b2a9909b923812fda20c97fd4
Matthew Kevork <matthew.kevork@hotmail.com>
2020-10-23 23:33:21 -0400

    finished section 160

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,9 +1,56 @@
 const socket = io()
 
-socket.on('countUpdated', (count) => {
-    console.log('The count has been updated', count)
+//elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+
+socket.on('message', (message) => {
+    console.log(message)
 })
 
-document.querySelector("#increment").addEventListener('click', () => {
-    socket.emit('increment')
+$messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
+
+    const message = e.target.elements.message.value
+
+    socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        if (error) {
+            return console.log(error)
+        }
+        console.log('message delivered')
+    })
+
+    
+})
+
+$sendLocationButton.addEventListener('click', (e) => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation i not supported by your browser.')
+    }
+
+    $sendLocationButton.setAttribute('disabled', 'disabled')
+    
+    const location = navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', { 
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, (error) => {
+            $sendLocationButton.removeAttribute('disabled')
+
+            if (error) {
+                return console.log(error)
+            }
+    
+            console.log('location shared')
+        })
+    })
 })
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http')
 const path = require('path')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -12,16 +13,30 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-let count = 0
-
 io.on('connection', (socket) => {
     console.log('New Websocket connection')
     
-    socket.emit('countUpdated', count)
+    socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message', 'A new user has joined')
 
-    socket.on('increment', () => {
-        count++
-        io.emit('countUpdated', count)
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+        
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed')
+        }
+
+        io.emit('message', message)
+        callback()
+    })
+
+    socket.on('sendLocation', (location, callback) => {
+        io.emit('message', `https://google.com/maps?q${location.latitude},${location.longitude}`)
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left')
     })
 })
 
